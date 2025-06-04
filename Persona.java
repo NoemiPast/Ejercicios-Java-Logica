ed2f5edb9feb6989dabcfd4e1bdbb5dac12add7d
Noemi <lic.noemipastrana@gmail.com>
2025-06-04 11:27:23 -0600

    Actualizaciones

---
+++ Persona.java
@@ -0,0 +1,18 @@
+public class Persona {
+    //Todas las personas pueden hacer muchas cosas, las personas tienen nombre, edad
+    //etc, asi que vamos a simular algunas actividades mediante metodos.
+
+    //Una buena practica es declarar variables en la parte mas alta del codigo
+
+    int edad = 20;
+
+    void mayorDeEdad(){
+       if (edad > 18){
+           System.out.println("Eres un mayor de edad :D");
+       }else{
+           System.out.println("Eres un menor de edad :c");
+
+
+       }
+    }
+}
